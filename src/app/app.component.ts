f3e8acd50d4be1ebbe59d981d13ecac20aba74e2
Jeymerson <jeymerson.jss@gmail.com>
2024-11-03 16:32:16 -0300

    components createds with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {HomeComponent} from './pages/home/home.component'
+//remover
+import {BigCardComponent} from "./components/big-card/big-card.component"
+import {MenuBarComponent} from "./components/menu-bar/menu-bar.component"
+import {MenuTitleComponent} from "./components/menu-title/menu-title.component"
+import {SmallCardComponent} from "./components/small-card/small-card.component"
+// até aqui
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, BigCardComponent, MenuBarComponent, SmallCardComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
